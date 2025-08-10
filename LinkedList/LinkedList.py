88e15e15a4a391a2901a6ce021d09eff8328ef0f
juanjerrah <juanjerrah15@gmail.com>
2025-08-10 20:25:08 -0300

    feat: creating pop and prepend methods

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -27,9 +27,49 @@ class LinkedList:
             self.tail = new_node
 
         self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        curr = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            pre = None
+            while curr.next is not None:
+                pre = curr
+                curr = curr.next
+            
+            self.tail = pre
+            pre.next = None
+        self.length -= 1
+        return curr.value
+
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
 
 
 ll = LinkedList(1)
 ll.append(2)
+ll.append(6)
+ll.append(3)
+ll.prepend(9)
+print("Before pop")
 ll.print()
+print("After pop")
+print("Removido: ", ll.pop())
+ll.print()
+
 
